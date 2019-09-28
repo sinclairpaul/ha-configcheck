9f7d58f5731b76ad773477e6f712655b80fb1df9
Paul Sinclair <sinclairpaul@hotmail.com>
2019-09-27 20:07:38 -0400

    🔨Update secrest options

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 
 sh -c "python3 -m homeassistant --version"
-sh -c "cp ci_secrets.yaml secrets.yaml"
+sh -c "cp $* secrets.yaml"
 sh -c "python3 -m homeassistant \
         --config ./ \
         --script check_config \
